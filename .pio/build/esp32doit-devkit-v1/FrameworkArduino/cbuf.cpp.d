5ee3593c16eed118223daa15685a60cd171e6192
jean-ka78 <68398640+jean-ka78@users.noreply.github.com>
2022-08-15 11:20:25 +0200

    fix

--- .pio/build/esp32doit-devkit-v1/FrameworkArduino/cbuf.cpp.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32doit-devkit-v1/FrameworkArduino/cbuf.cpp.o: \
- /home/yevhenii/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
- /home/yevhenii/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
